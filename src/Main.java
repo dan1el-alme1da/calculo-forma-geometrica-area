e9d8055db8d6bdafebaf31fe8d6b7f69f4ddd2c7
daniel almeida <134938468+dan1el-alme1da@users.noreply.github.com>
2025-03-13 10:50:12 -0300

    feat: main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+       Retangulo retangulo = new Retangulo();
+       retangulo.areaRetangulo();
+       
+       
+       Circulo circulo = new Circulo();
+       circulo.areaCirculo();
+
+        
+    }
+}
